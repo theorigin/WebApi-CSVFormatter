508ca164047d6f52d16c50b94645c22b3a3341a8
andyrobinson@hotmail.com <andyrobinson@hotmail.com>
2015-09-27 23:04:05 +0100

    Initial commit

---
+++ src/CsvResponse/CsvFormatter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace VS.CsvResponse
+{
+    public class CsvFormatter : BufferedMediaTypeFormatter
+    {
+        static readonly char[] SpecialChars = { ',', '\n', '\r', '"' };
+
+        private readonly string _fields;
+
+        private CsvFormatter(HttpRequestMessage request) : this()
+        {
+            if (request == null) return;
+
+            var queryOption = HttpUtility.ParseQueryString(request.RequestUri.Query)["fields"];
+
+            _fields = queryOption ?? "*";
+        }
+
+        public CsvFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/csv"));
+        }
+
+        public override MediaTypeFormatter GetPerRequestFormatterInstance(Type type, HttpRequestMessage request, MediaTypeHeaderValue mediaType)
+        {
+            return new CsvFormatter(request);
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            return true;
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            return false;
+        }
+
+        public override void WriteToStream(Type type, object value, Stream writeStream, HttpContent content)
+        {
+            using (var writer = new StreamWriter(writeStream))
+            {
+                var dt = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(value), (typeof(DataTable)));
+
+                var cols = dt.Columns.Cast<DataColumn>().Where(column => _fields.IndexOf(column.ColumnName, StringComparison.OrdinalIgnoreCase) >= 0 || _fields == "*").Select(x => x.ColumnName).ToList();
+
+                var columnNames = cols.Select(column => "\"" + column.Replace("\"", "\"\"") + "\"").ToArray();
+
+                writer.WriteLine(string.Join(",", columnNames));
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    var fields = cols.Select(column => Escape(row[column].ToString())).ToList();
+
+                    writer.WriteLine(string.Join(",", fields));
+                }
+            }
+        }
+        
+        private static string Escape(object o)
+        {
+            if (o == null)
+                return "";
+            
+            var field = o.ToString();
+            return field.IndexOfAny(SpecialChars) != -1 ? $"\"{field.Replace("\"", "\"\"")}\"" : field;
+        }
+    }
+}
---
+++ test/CsvResponse.Test/CsvFormatterTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using NUnit.Framework;
+using VS.CsvResponse;
+
+namespace CsvResponse.Test
+{
+    [TestFixture]
+    public class CsvFormatterTests
+    {
+        [Test]
+        public void CorrectDataReturned()
+        {
+            // Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost?fields=Id,Name(");
+            var value = GetData().Take(1);
+
+            // Act
+            var result = Test(request, value);
+
+            Assert.That(result, Is.EqualTo("\"Id\",\"Name\"\r\n1,Product 1\r\n"));
+        }
+
+        [Test]
+        public void SpecialCharsAreEscaped()
+        {
+            // Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost?fields=Id,Name(");
+            var value = GetData().Skip(1).Take(1);
+
+            // Act
+            var result = Test(request, value);
+
+            Assert.That(result, Is.EqualTo("\"Id\",\"Name\"\r\n2,\"Product,2 - \"\"It's great\"\"\"\r\n"));
+        }
+
+        [Test]
+        public void AllFieldsReturned()
+        {
+            // Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost?fields=*");
+            var value = GetData().Take(1);
+
+            // Act
+            var result = Test(request, value);
+
+            Assert.That(result, Is.EqualTo("\"Id\",\"Name\",\"Price\",\"StockQuantity\",\"LastOrderDate\"\r\n1,Product 1,1.23,123,27/09/2015 00:00:00\r\n"));
+        }
+
+        private static string Test<T>(HttpRequestMessage request, T value, bool ignoreCase = false)
+        {
+            var formatter = new CsvFormatter().GetPerRequestFormatterInstance(null, request, null);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                formatter.WriteToStreamAsync(typeof(T), value, memoryStream, null, null);
+
+                memoryStream.Flush();
+                memoryStream.Position = 0;
+
+                var sr = new StreamReader(memoryStream);
+                return sr.ReadToEnd();
+            }
+        }
+        
+        private static IEnumerable<Product> GetData()
+        {
+            yield return new Product
+            {
+                Id = 1,
+                Name = "Product 1"              ,
+                LastOrderDate = DateTime.UtcNow.Date,
+                Price = (decimal)1.23,
+                StockQuantity = 123
+            };
+
+            yield return new Product
+            {
+                Id = 2,
+                Name = "Product,2 - \"It's great\"",
+                LastOrderDate = DateTime.UtcNow.Date,
+                Price = (decimal)1.23,
+                StockQuantity = 123
+            };
+
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public DateTime LastOrderDate { get; set; }
+    }
+}
